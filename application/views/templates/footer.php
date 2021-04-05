6de5efb40e961e875da23da3d386c11658eb41b4
Alexandrof Ilya <alexandroff.ok@gmail.com>
2021-04-05 17:56:39 +0300

    first commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,32 @@
+ <!-- Footer start -->  
+       </div>
+          <?php $this->load->view('templates/menu', $news, $films); ?>
+
+       </div>
+
+      </div>
+
+      <div class="clear"></div>
+
+    </div>
+
+
+    <footer>
+      
+      <div class="container">
+        <p class="text-center"> <a href="https://vk.com/burbetovv">© Dyatlovo Inc. 2000−2021 </a> </p>
+      </div>
+
+    </footer>
+
+
+
+    <script src="//code-ya.jivosite.com/widget/UkpriiRDJO" async></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+<!-- Footer end -->
